6c19fe89d4ccc2239f8df1a845118756fbd96ea0
sergeyblagodarnyy <sergey.blagodarnyy@faramax.com>
2023-03-13 15:39:26 +0100

    Draft for printf debug output

--- sw/overall/logger/logger.h
+++ sw/overall/logger/logger.h
@@ -6,3 +6,12 @@
 #define RELAY_LOG_WARNING(...)                        { }
 #define RELAY_LOG_ERROR(...)                          { }
 #define RELAY_LOG_CRITICAL(...)                       { }
+
+//#include <cstdio>
+
+//#define RELAY_LOG_TRACE(module_enum, string, ...)      {fprintf(stdout,  string "\n", __VA_ARGS__); fflush(stdout);}
+//#define RELAY_LOG_DEBUG(module_enum, string, ...)      {fprintf(stdout,  string "\n", __VA_ARGS__); fflush(stdout);}
+//#define RELAY_LOG_INFO(module_enum, string, ...)       {fprintf(stdout,  string "\n", __VA_ARGS__); fflush(stdout);}
+//#define RELAY_LOG_WARNING(module_enum, string, ...)    {fprintf(stdout,  string "\n", __VA_ARGS__); fflush(stdout);}
+//#define RELAY_LOG_ERROR(module_enum, string, ...)      {fprintf(stdout,  string "\n", __VA_ARGS__); fflush(stdout);}
+//#define RELAY_LOG_CRITICAL(module_enum, string, ...)   {fprintf(stdout,  string "\n", __VA_ARGS__); fflush(stdout);}
